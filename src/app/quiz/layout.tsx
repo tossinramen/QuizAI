ca259050e4898b8a446f90cc4efa7d0b7cb6e5e9
Tahsin Rahman <tr174@duke.edu>
2025-03-05 04:06:39 -0500

    hc

--- src/app/quiz/layout.tsx
+++ src/app/quiz/layout.tsx
@@ -2,7 +2,7 @@ export default function Layout({ children }: {
     children: React.ReactNode
 }) {
     return(
-        <div>
+        <div className="flex flex-col flex-1 max-w-96 w-full m-auto h-screen gap-6">
             {children}
         </div>
     )
--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,13 +1,30 @@
+"use client";
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
+    const [started, setStarted] = useState(false);
+    const handleNext = () => {
+        setStarted(true);
+    }
   return (
     <div className="flex flex-col flex-1">
     <main className="flex justify-center flex-1">
-      <h1 className="text-6xl font-bold">Welcome to the quiz page 👋</h1>
+      {!started ? <h1 className="text-3xl font-bold">Welcome to the quiz page 👋</h1>
+      : (
+        <div >
+            <h2 className="text-3xl font-bold">What is your level of understanding of React?</h2>
+            <div>
+                <Button>Beginner</Button>
+                <Button>Intermediate</Button>
+                <Button>Advanced</Button>
+                <Button>Expert</Button>
+            </div>
+        </div>
+      )}
     </main>
     <footer className="footer pb-9 px-6 relative mb-0">
-      <Button>Start</Button>
+      <Button onClick={handleNext}>{!started ? 'Start' : 'Next'}</Button>
     </footer>
     </div>
   )
