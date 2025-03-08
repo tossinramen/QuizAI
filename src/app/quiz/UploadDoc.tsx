138cba2bd9a046113ee6a15de68d4a6b6f90495d
Tahsin Rahman <tr174@duke.edu>
2025-03-08 12:04:41 -0500

    file structure

--- src/app/quiz/UploadDoc.tsx
+++ src/app/quiz/UploadDoc.tsx
@@ -4,10 +4,30 @@ import {Button} from "@/components/ui/button";
 const UploadDoc = () => {
     const [document, setDocument] = useState<Blob | File | null | undefined>(null);
     const [isLoading, setIsLoading] = useState<boolean>(false);
+    const[error, setError] = useState<string>("");
+
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        if (!document) {
+            setError("Please upload the document first");
+            return;
+        }
         setIsLoading(true);
-        console.log(document);
+        const formData = new FormData();
+        formData.append("pdf", document as Blob);
+        try {
+            const res = await fetch("/api/quiz/generate", {
+                method: "POST",
+                body: formData
+            });
+            if (res.status === 200) {
+                console.log("quiz generated successfully");
+            }
+        } catch(e) {
+            console.log("error while generating", e);
+        }
+        setIsLoading(false);
+
     }
     return(
         <div className="w-full">
@@ -20,6 +40,7 @@ const UploadDoc = () => {
 <input type="file" id="document" className="relative block w-full h-full z-50 opacity-0" onChange={(e) => setDocument(e?.target?.files?.[0])}/>
 
 </label>
+{error ? <p className='text-red-600'>{error}</p> : null}
 <Button size="lg" className='mt-2' type="submit">Generate Quiz 🚀</Button>
 <input type="file" id="docuent" className="relative block w-full h-full z-50 opacity-0"/>
             </form>
