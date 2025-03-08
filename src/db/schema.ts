f6fc8395e4b0b6b11052e17e3e0d0c752012f018
Tahsin Rahman <tr174@duke.edu>
2025-03-08 12:40:59 -0500

    finish defining schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -10,3 +10,44 @@ import {
 } from "drizzle-orm/pg-core";
 import type { AdapterAccount } from "@auth/core/adapters";
 import { relations } from "drizzle-orm";
+
+export const quizzes = pgTable("quizzes", {
+    id: serial("id").primaryKey(),
+    name: text("name"),
+    description: text("description"),
+    userId: text("user_id"),
+});
+
+export const quizzesRelations = relations(quizzes, ({ many, one }) => ({
+    questionss: many(questions),
+}));
+
+
+export const questions = pgTable("questions", {
+    id: serial("id").primaryKey(),
+    questionText: text("question_text"),
+    quizId: integer("quiz_id"),
+});
+
+export const questionsRelations = relations(questions, ({ one, many}) => ({
+    quiz: one(quizzes, {
+        fields: [questions.quizId],
+        references: [quizzes.id],
+    }),
+    answers: many(questionAnswers),
+}));
+
+export const questionAnswers = pgTable("answers", {
+    id: serial("id").primaryKey(),
+    questionId: integer("question_id"),
+    answerText: text("answer_text"),
+    isCorrect: boolean("is_correct"),
+});
+
+export const questionAnswerRelations = relations(
+    questionAnswers, ({ one }) => ({
+    questions: one(questions, {
+        fields: [questionAnswers.questionId],
+        references: [questions.id]
+    })
+}))
