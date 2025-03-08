e7f7e87130670bff9b5895d4cb53e2363d35edc0
Tahsin Rahman <tr174@duke.edu>
2025-03-08 08:31:04 -0500

    upload doc done

--- src/app/quiz/UploadDoc.tsx
+++ src/app/quiz/UploadDoc.tsx
@@ -1,10 +1,27 @@
 "use client" // render on client side not server
+import { useState } from 'react';
+import {Button} from "@/components/ui/button";
 const UploadDoc = () => {
+    const [document, setDocument] = useState<Blob | File | null | undefined>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setIsLoading(true);
+        console.log(document);
+    }
     return(
-        <div>
-            <form>
-<label htmlFor="document"></label>
-<input type="file" id="document" />
+        <div className="w-full">
+            <form className="w-full" onSubmit={handleSubmit}>
+<label htmlFor="document" className="bg-secondary w-full flex h-20 rounded-md border-4 border-dashed border-blue-900 relative">
+    <div className="absolute inset-0 m-auto flex justify-center items-center">
+        {document && document?.name? document.name : "Choose a file"}
+
+    </div>
+<input type="file" id="document" className="relative block w-full h-full z-50 opacity-0" onChange={(e) => setDocument(e?.target?.files?.[0])}/>
+
+</label>
+<Button size="lg" className='mt-2' type="submit">Generate Quiz 🚀</Button>
+<input type="file" id="docuent" className="relative block w-full h-full z-50 opacity-0"/>
             </form>
         </div>
     )
