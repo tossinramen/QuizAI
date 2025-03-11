c620c06e9d77c1e5c8052400da90530b6d9b4ac8
Tahsin Rahman <tr174@duke.edu>
2025-03-11 14:08:49 -0400

    layout

--- src/app/(user)/dashboard/page.tsx
+++ src/app/(user)/dashboard/page.tsx
@@ -1,4 +1,24 @@
-const page = () => {
+import { db } from "@/db";
+import { eq } from "drizzle-orm";
+import { quizzes } from "@/db/schema";
+import { auth } from "@/auth";
+
+
+const page = async () => {
+    const session = await auth();
+    const userId = session?.user?.id;
+
+    if (!userId) {
+        return (<p>User not found</p>)
+    };
+
+    const userQuizzes = await db.query.quizzes.findMany(
+        {
+            where: eq(quizzes.userId, userId)
+
+        }
+    );
+    console.log(userQuizzes)
     return(
         <div>dashboard</div>
     )
