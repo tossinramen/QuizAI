d9cc5d078d869e324fdfe5a916c0d7fc11513a65
Tahsin Rahman <tr174@duke.edu>
2025-03-21 16:20:43 -0400

    page.tsx

--- src/app/(user)/billing/page.tsx
+++ src/app/(user)/billing/page.tsx
@@ -1,1 +1,30 @@
 import ManageSubscription from "./ManageSubscription";
+import React from 'react'
+import { auth, signIn } from '@/auth';
+import { db } from '@/db';
+import { users } from '@/db/schema';
+import { eq } from 'drizzle-orm';
+
+
+const page = async () => {
+    const session = await auth();
+    if (!session || !session.user || !session.user.id){
+        signIn();
+        return null;
+    }
+    const user = await db.query.users.findFirst({
+        where: eq(users.id, session.user.id)
+    })
+    const plan = user?.subscribed ? 'premium' : 'free';
+
+
+    return(
+        <div className="p-4 border rounded-md">
+            <h1 className="text-4xl mb-3">Subscription Details</h1>
+            <p>You are currently on a { plan } plan</p>
+            <ManageSubscription />
+        </div>
+    )
+}
+
+export default page; 
