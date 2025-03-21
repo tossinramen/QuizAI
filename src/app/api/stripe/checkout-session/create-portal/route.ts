81ce578ba65944e393b9f18224c161e09883d285
Tahsin Rahman <tr174@duke.edu>
2025-03-21 16:12:45 -0400

    need to add billing page

--- src/app/api/stripe/checkout-session/create-portal/route.ts
+++ src/app/api/stripe/checkout-session/create-portal/route.ts
@@ -7,5 +7,74 @@ import { eq } from "drizzle-orm";
 export async function POST(
     req: Request
 ) {
-    const session = await au
+    const session = await auth();
+    const userId = session?.user?.id;
+    if (!userId) {
+        return new Response(
+            JSON.stringify({
+                error: "Unauthorized",
+            }),
+            {
+                status: 401,
+            }
+        );
+    }
+    const user = await db.query.users.findFirst({
+        where: eq(users.id, userId)
+    });
+    if(!user){
+        return new Response(
+            JSON.stringify({
+                error: "User not found",
+            }),
+            {
+                status: 404,
+            }
+        );
+
+    }
+    
+    let customer;
+
+    if (user?.stripeCustomerId){
+        customer = {
+            id: user.stripeCustomerId
+        };
+    }
+    else{
+        const customerData: {
+            metadata: {
+                dbId: string
+            };
+
+        } = {
+            metadata: {
+                dbId: userId
+            }
+        }
+
+        const response = await stripe.customers.create(
+            customerData
+        );
+        customer = { id: response.id };
+        await db
+        .update(users)
+        .set({
+            stripeCustomerId: customer.id
+        })
+        .where(eq(users.id, userId));
+    }
+
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+
+    const url = await stripe.billingPortal.sessions.create({
+        customer: customer.id,
+        return_url: `${baseUrl}/billing`
+    })
+    return new Response(
+        JSON.stringify({ url }),
+     {
+        status: 200,
+    });
+    
 }
