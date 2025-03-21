9988dd1e98ba86bfee947e7c75cfb17fd688d3e8
Tahsin Rahman <tr174@duke.edu>
2025-03-21 04:23:21 -0400

    test stripe

--- src/app/api/stripe/checkout-session/route.ts
+++ src/app/api/stripe/checkout-session/route.ts
@@ -22,5 +22,88 @@ req: Request
             }
         )
     }
+    const user = await db.query.users.findFirst({
+        where: eq(users.id, userId)
+    });
+    let customer;
 
+    if (user?.stripeCustomerId){
+        customer = {
+            id: user.stripeCustomerId
+        };
+    }
+    else{
+        const customerData: {
+            metadata: {
+                dbId: string
+            };
+
+        } = {
+            metadata: {
+                dbId: userId
+            }
+        }
+
+        const response = await stripe.customers.create(
+            customerData
+        );
+        customer = { id: response.id };
+        await db
+        .update(users)
+        .set({
+            stripeCustomerId: customer.id
+        })
+        .where(eq(users.id, userId));
+    }
+
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+    try{
+        const session = await stripe.checkout.sessions.create(
+            {
+                success_url: `${baseUrl}/payment/success`,
+                customer: customer.id,
+                payment_method_types: [
+                    "card"
+                ],
+                line_items: [
+                    {
+                        price,
+                        quantity
+                    }
+                ],
+                mode: "subscription"
+            }
+        )
+        if (session) {
+            return new Response(
+                JSON.stringify({
+                    sessionId: session.id
+                }),
+                {
+                    status: 200
+                }
+            )
+        }
+        else{
+            return new Response(
+                JSON.stringify({
+                    error: "Failed to create a session"
+                }),
+                {
+                    status: 500
+                }
+            )
+        }
+    }
+    catch(error) {
+        console.log("Error creating checkout session", error);
+        return new Response(
+            JSON.stringify({
+                error,
+            }),
+            {
+                status: 500,
+            }
+        )
+    }
 }
