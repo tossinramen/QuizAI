2c19a4d7b37bf256b9071acac056089fce2115c8
Tahsin Rahman <tr174@duke.edu>
2025-03-24 13:18:27 -0400

    Landing page edits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,22 @@
 import { Button } from "@/components/ui/button";
-
+import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1">
     <main className="flex justify-center flex-1">
-      <h1 className="text-6xl font-bold">Welcome to the quiz page 👋</h1>
+      <div  className="items-center flex flex-col sm:flex-row gap-20 justify-end mx-auto p-10 w-full sm:py-20 sm:w-[1000px]">
+        <div>
+          <Image src="/images/owl-landing-no-bg.png" width="400" height="400" alt="owl" />
+        </div>
+        <div className="text-center flex gap-6 flex-col">
+          <h1 className="text-3xl font-bold">Get quizzed about anything!</h1>
+          <h3 className="text-sm">Upload documents, and easily generate your quizzes using AI.</h3>
+          <Button variant="neo" className="mt-4 h-14 text-white">Get Started</Button>
+        </div>
+      </div>
+      
     </main>
-    <footer className="footer pb-9 px-6 relative mb-0">
-      <Button>Start</Button>
-    </footer>
+    
     </div>
   )
 }
